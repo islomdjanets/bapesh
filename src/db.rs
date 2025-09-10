46e1d8a7eec54c6f395c79d38e89d558ff73c273
islomdjanets <0djanets@gmail.com>
2025-09-10 21:38:00 +0500

    dfdf

--- src/db.rs
+++ src/db.rs
@@ -276,7 +276,7 @@ pub async fn create_table(name: &str, schema: &JSON, pool: &sqlx::Pool<sqlx::Pos
     let properties = generate_properties(schema, pool).await;
     println!("Properties: {}", properties);
 
-    let query = &format!("CREATE TABLE {} ({})", name, properties);
+    let query = &format!("CREATE TABLE {} ({});", name, properties);
     println!("Create table query: {}", query);
 
     sqlx::query(query)
