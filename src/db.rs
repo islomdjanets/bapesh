2567274ac4dc16f487ac461d3d4c0d2dd1101dac
islomdjanets <0djanets@gmail.com>
2025-09-30 21:00:39 +0500

    df

--- src/db.rs
+++ src/db.rs
@@ -891,7 +891,7 @@ pub async fn update(table: &str, key: &str, id: i64, value: &str, pool: &Pool, c
     Ok(())
 }
 
-pub async fn update_record(table: &str, key: &str, column: i64, id: &str, value: &str, pool: &Pool, cast: &str) -> Result<(), StdError> {
+pub async fn update_record(table: &str, key: &str, column: &str, id: &str, value: &str, pool: &Pool, cast: &str) -> Result<(), StdError> {
     let query = &format!("UPDATE {} SET {} = $1::{} WHERE {} = $2", table, key, cast, column);
 
     println!("Update query: {}", query);
